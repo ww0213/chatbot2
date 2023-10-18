902187a9a964696ab46644f8addcbac92786bd61
User <User@WS2.r1802.tciii.org.tw>
2023-10-18 10:11:56 +0800

    231018

--- chatbot/src/main/java/com/tw/controller/HomeController.java
+++ chatbot/src/main/java/com/tw/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        // 這裡的 "chat" 指的是 chat.jsp，根據您在 application.properties 中的設定
+        
         return "index";
     }
 }
